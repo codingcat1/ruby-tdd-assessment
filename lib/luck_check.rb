f8823f2899edc7d3d6c5986a1460bdd31190d992
Catherine Tran <tranc489@gmail.com>
2014-08-10 19:58:23 -0700

    make functionality to take a string of numbers with an odd number of elements and return as an array with the middle item removed.

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,7 +1,14 @@
-def luck_check input
+def luck_check(input)
 
-  numbers = input.split("").map(&:to_i)
+  numbers_array = input.split("").map(&:to_i)
+  numbers_length = numbers_array.length
 
+  if numbers_length.odd?
+    numbers_array.slice!(((numbers_length-1)/2))
+  end
+
+
+ numbers_array
 end
 
 p luck_check('12345')
--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -4,7 +4,11 @@ require 'luck_check'
 describe 'luck_check' do
 
   it "should take an inputted number string and return it as an array" do
-    luck_check('12345').should (eq([1,2,3,4,5]))
+    luck_check('1234').should (eq([1, 2, 3, 4]))
+  end
+
+  it "should take a string of numbers with an odd number of elements and return as an array with the middle item removed" do
+    luck_check('12345').should (eq([1, 2, 4, 5]))
   end
 
 end
