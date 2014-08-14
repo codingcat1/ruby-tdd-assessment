4a7a8b7a53e58c1c474aac1c615c6d1a837f0d4d
Catherine Tran <tranc489@gmail.com>
2014-08-14 10:54:17 -0700

    add note for previous commit: delete unnecessary specs.

--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -3,11 +3,11 @@ require 'luck_check'
 
 describe 'luck_check' do
 
-  it "should return true if the sum of left half of numbers_array is equal to the sum of right half of numbers_array" do
+  it "should return true if sum of left half of numbers_array is equal to sum of right half of numbers_array" do
     expect(luck_check('12312')).to eq true
   end
 
-  it "should return false if the sum of left half of numbers_array is NOT equal to the sum of right half of numbers_array" do
+  it "should return false if sum of left half of numbers_array is NOT equal to sum of right half of numbers_array" do
     expect(luck_check('12345')).to eq false
   end
 
