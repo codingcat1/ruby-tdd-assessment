425b3c366b31503b021290808e0d1b36d5f8cfa0
Catherine Tran <tranc489@gmail.com>
2014-08-14 09:32:21 -0700

    reformat README.md.  Change specs from 'should' to 'expect'.

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -2,12 +2,13 @@ def luck_check(input)
 
   numbers_array = input.split("").map(&:to_i)
   numbers_length = numbers_array.length
-  half = numbers_length/2
+
 
   if numbers_length.odd?
-    numbers_array.slice!(((numbers_length-1)/2))
+    numbers_array.slice!((numbers_length-1)/2)
   end
 
+  half = numbers_length/2
   left_half = numbers_array[0..(half-1)]
   right_half = numbers_array[half..-1]
 
--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -4,15 +4,15 @@ require 'luck_check'
 describe 'luck_check' do
 
   it "should take an inputted number string and return it as an array" do
-    luck_check('1234').should (eq([1, 2, 3, 4]))
+    expect(luck_check('1234')).to eq([1, 2, 3, 4])
   end
 
   it "should take a string of numbers with an odd number of elements and return as an array with the middle item removed" do
-    luck_check('12345').should (eq([1, 2, 4, 5]))
+    expect(luck_check('12345')).to eq([1, 2, 4, 5])
   end
 
   it "should divide the array of numbers into 2 equal halves" do
-    luck_check('12345').should (eq([[1,2],[4,5]]))
+    expect(luck_check('12345')).to eq([[1,2],[4,5]])
   end
 
 end
